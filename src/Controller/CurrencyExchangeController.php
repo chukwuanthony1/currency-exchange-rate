df2bef984ce01558c72e406875cbc24ba712ebe2
Anthony Chukwu <anthony.chukwu@wis-ltd.net>
2023-04-21 20:42:27 +0200

    setup controller and services

---
+++ src/Controller/CurrencyExchangeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CurrencyExchange;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CurrencyExchangeController extends AbstractController
+{
+
+    public function __construct(
+        private CurrencyExchange $currencyExchange,
+    ) {
+    }
+
+    #[Route('/api/exchange-rates', name: 'app_currency_exchange')]
+    public function get_exchange_rates(Request $request): JsonResponse
+    {
+
+        // dump($request->query->get('base_currency'));
+        // dump($request->query->get('target_currencies'));
+        $base_currency = $request->query->get('base_currency');
+        $target_currencies = $request->query->get('target_currencies');
+        $target_currencies = explode(",", $target_currencies);
+
+        $response = $this->currencyExchange->fetchExchangeRates($base_currency, $target_currencies);
+        return $this->json([
+            'message' => $base_currency,
+            'path' => 'src/Controller/CurrencyExchangeController.php',
+            'data' => $target_currencies,
+            'response' => $response
+        ]);
+    }
+}
--- src/Service/CurrencyExchange.php
+++ src/Service/CurrencyExchange.php
@@ -1,19 +1,23 @@
-<?php 
+<?php
 
 namespace App\Service;
 
 class CurrencyExchange
 {
-    public function getHappyMessage(): string
+    public function fetchExchangeRates(string $base_currency, array $target_currencies): array
     {
-        $messages = [
-            'You did it! You updated the system! Amazing!',
-            'That was one of the coolest updates I\'ve seen all day!',
-            'Great work! Keep going!',
+        $response = [];
+        foreach ($target_currencies as $target_currency) {
+            $exchange_rate = $this->fetchSingleExchangeRate($base_currency, $target_currency);
+            array_push($response, $exchange_rate);
+        }
+        return $response;
+    }
+
+    private function fetchSingleExchangeRate(string $base_currency, string $target_currency): array
+    {
+        return [
+            $base_currency => $target_currency
         ];
-
-        $index = array_rand($messages);
-
-        return $messages[$index];
     }
 }
